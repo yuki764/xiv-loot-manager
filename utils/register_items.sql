a212735faca5ba81f5c100a84fa1e7362dfec565
Setuu <setuu@neigepluie.net>
2022-09-06 00:51:55 +0900

    add SQL to register items information

---
+++ utils/register_items.sql
@@ -0,0 +1,18 @@
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・イヤリングチェスト【ILv630】", "アクセ/耳");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ネックレスチェスト【ILv630】", "アクセ/首");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ブレスレットチェスト【ILv630】", "アクセ/腕");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・リングチェスト【ILv630】", "アクセ/指");
+
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ヘッドギアチェスト【ILv630】", "防具/頭");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ボディギアチェスト【ILv630】", "防具/胴");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ハンドギアチェスト【ILv630】", "防具/手");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・レッグギアチェスト【ILv630】", "防具/脚");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・フットギアチェスト【ILv630】", "防具/靴");
+
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("アビュッソス・ウェポンチェスト【ILv635】", "武器");
+
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("月光の硬化薬", "強化/アクセ");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("月光の強化繊維", "強化/防具");
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("月光の強化薬", "強化/武器");
+
+INSERT INTO ffxiv_loot.pandaemonium_abyssos_items VALUES ("極薄型トームストーン", "素材/武器交換");
